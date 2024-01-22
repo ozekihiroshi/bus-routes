88d2df940fcf699362209a2914ae58cb5cd4c6f6
Hiroshi Ozekie <ozeki.hiroshi@gmail.com>
2024-01-22 12:54:16 +0900

    js rename

--- my-leaflet-plugin.php
+++ my-leaflet-plugin.php
@@ -12,7 +12,7 @@ function my_leaflet_map_enqueue_scripts()
     wp_enqueue_style('leaflet-css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css');
     wp_enqueue_script('leaflet-js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', array(), null, true);
     wp_enqueue_script('leaflet-gpx-js', 'https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/gpx.min.js', array('leaflet-js'), null, true);
-    wp_enqueue_script('my-leaflet-map-js', plugin_dir_url(__FILE__) . 'assets/js/my-leaflet-map-v1.0.6.js', array('leaflet-js', 'leaflet-gpx-js'), null, true);
+    wp_enqueue_script('my-leaflet-map-js', plugin_dir_url(__FILE__) . 'assets/js/my-leaflet-map-v1.0.7.js', array('leaflet-js', 'leaflet-gpx-js'), null, true);
 }
 
 add_action('wp_enqueue_scripts', 'my_leaflet_map_enqueue_scripts');
