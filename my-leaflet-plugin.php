30e931722ad23041d58c41a059b0e39dfd60ab04
Hiroshi Ozeki <hiroshiozeki@MacBook-Pro.local>
2024-01-22 05:29:03 +0200

    file format

--- my-leaflet-plugin.php
+++ my-leaflet-plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: My Leaflet Map Plugin
  * Description: A simple plugin to display GPX tracks on a Leaflet map.
@@ -6,7 +7,8 @@
  * Author: Hiroshi Ozeki
  */
 
-function my_leaflet_map_enqueue_scripts() {
+function my_leaflet_map_enqueue_scripts()
+{
     wp_enqueue_style('leaflet-css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css');
     wp_enqueue_script('leaflet-js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', array(), null, true);
     wp_enqueue_script('leaflet-gpx-js', 'https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/gpx.min.js', array('leaflet-js'), null, true);
@@ -15,7 +17,8 @@ function my_leaflet_map_enqueue_scripts() {
 
 add_action('wp_enqueue_scripts', 'my_leaflet_map_enqueue_scripts');
 
-function my_leaflet_map_shortcode() {
+function my_leaflet_map_shortcode()
+{
     $html = '<div id="my-leaflet-map" style="height: 450px;"></div>';
     return $html;
 }
